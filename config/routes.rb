2c4aeffbce2e00005625f63cf30ddb8f287fe4d1
roger chat <mattrogerpelosi@gmail.com>
2021-10-20 13:50:12 -0400

    commit tree 1ba331fd62e995d42a88498065bced46e9dd7b6b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/login', to: 'session#create'
+
+  post '/signup', to: 'users#create'
+
+  get '/me', to: 'users#show'
+
+  delete '/logout', to: 'session#destroy'
+
 end
